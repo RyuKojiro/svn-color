a646a3563369837b155e4df86d364d1661983728
Daniel Loffgren <daniel@loffgren.org>
2015-10-15 01:00:01 -0700

    Rely on local PATH for automatic determination of svn location, instead of hardcoding it

--- svn-color.py
+++ svn-color.py
@@ -44,7 +44,7 @@ def colorize(line):
 
 if __name__ == '__main__':
     command = sys.argv
-    command[0] = '/usr/bin/svn'
+    command[0] = 'svn'
     
     if len(command) > 1:
         subcommand = (command[1], '')[len(command) < 2]
